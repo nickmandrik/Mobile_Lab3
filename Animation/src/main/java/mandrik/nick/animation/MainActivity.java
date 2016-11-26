dac37fe88d91091bf07225bee8570a2f91a0a6ab
zvermandr <zvermandr@gmail.com>
2016-11-26 15:46:53 +0300

    Add files via upload

---
+++ Animation/src/main/java/mandrik/nick/animation/MainActivity.java
@@ -0,0 +1,57 @@
+package mandrik.nick.animation;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    private Button startFrameAnim;
+    private Button startTransformAnim;
+    private Button startCustomAnim;
+    private Button cancelAnim;
+    private ImageView animationView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startFrameAnim = (Button) findViewById(R.id.frameAnimationStart);
+        startTransformAnim = (Button) findViewById(R.id.transformAnimationStart);
+        startCustomAnim = (Button) findViewById(R.id.customAnimation);
+        cancelAnim = (Button) findViewById(R.id.cancelAnimation);
+        animationView = (ImageView) findViewById(R.id.animationView);
+
+        startFrameAnim.setOnClickListener(this);
+        startTransformAnim.setOnClickListener(this);
+        startCustomAnim.setOnClickListener(this);
+
+        cancelAnim.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        if (startFrameAnim.equals(v)) {
+            animationView.setBackgroundResource(R.drawable.frame_anim);
+            AnimationDrawable animation = (AnimationDrawable) animationView.getBackground();
+            animation.start();
+        } else if (startTransformAnim.equals(v)) {
+            animationView.setBackgroundResource(R.drawable.memas);
+            Animation transformAnimation = AnimationUtils.loadAnimation(this, R.anim.transform_anim);
+            animationView.startAnimation(transformAnimation);
+        } else if (cancelAnim.equals(v)) {
+            animationView.setBackgroundColor(Color.WHITE);
+        } else if (startCustomAnim.equals(v)) {
+            animationView.setBackgroundResource(R.drawable.memas);
+            Animation transformAnimation = AnimationUtils.loadAnimation(this, R.anim.my_anim);
+            animationView.startAnimation(transformAnimation);
+        }
+    }
+}
